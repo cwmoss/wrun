c12e5c1bdb99059df6b0609974ba311456ad9d57
Robert Wagner <rw@20sec.net>
2024-09-09 17:17:03 +0200

    move dbg function to runner for now

--- wrun.php
+++ wrun.php
@@ -1706,18 +1706,6 @@ class Response
 
 namespace wrun;
 
-function dbg($txt, ...$vars) {
-    // im servermodus wird der zeitstempel automatisch gesetzt
-    //	$log = [date('Y-m-d H:i:s')];
-    $log = [];
-    if (!is_string($txt)) {
-        array_unshift($vars, $txt);
-    } else {
-        $log[] = $txt;
-    }
-    $log[] = join(' ~ ', array_map(fn($v) => json_encode($v, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), $vars));
-    error_log(join(' ', $log));
-}
 
 #require_once("./runner.php");
 #require_once("./request.php");
@@ -1794,6 +1782,18 @@ namespace wrun;
     must be php 7.4 compatible (for now)
 */
 
+function dbg($txt, ...$vars) {
+    // im servermodus wird der zeitstempel automatisch gesetzt
+    //	$log = [date('Y-m-d H:i:s')];
+    $log = [];
+    if (!is_string($txt)) {
+        array_unshift($vars, $txt);
+    } else {
+        $log[] = $txt;
+    }
+    $log[] = join(' ~ ', array_map(fn($v) => json_encode($v, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), $vars));
+    error_log(join(' ', $log));
+}
 class runner {
 
     public string $base;
